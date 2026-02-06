5dda398cc4c832d7785f64568f311e125dbc3d06
jackbongmin <rrrgk12@gmail.com>
2026-02-06 21:22:18 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,29 @@
+
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+    //fastio();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+
+    int n;
+    cin >> n; 
+
+    cout << 1 << "\n"; 
+    cout << 0 << "\n"; 
+    
+    return 0;
+}
+
