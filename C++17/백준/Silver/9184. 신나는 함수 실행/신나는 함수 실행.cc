ed23366418518ee2d7245ccc6ad603c9d957e43a
jackbongmin <rrrgk12@gmail.com>
2026-02-22 19:55:21 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,70 @@
+
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+
+int memo[21][21][21];
+
+int w(int a, int b, int c) 
+{
+    if (a <= 0 || b <= 0 || c <= 0)
+    {
+        return 1;
+    }
+
+    if (a > 20 || b > 20 || c > 20)
+    {
+        return w(20, 20, 20);
+    }
+
+    if (memo[a][b][c] != 0)
+    {
+        return memo[a][b][c];
+    }
+
+    if (a < b && b < c) 
+    {
+        memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    }
+    else 
+    {
+        memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+
+    return memo[a][b][c];
+}
+
+
+
+int main() {
+    //fastio();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+
+    int a, b, c;
+
+    while (true) {
+        cin >> a >> b >> c;
+
+        if (a == -1 && b == -1 && c == -1)
+        {
+            break;
+        }
+
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << "\n";
+    }
+
+    return 0;
+    
+}
+
