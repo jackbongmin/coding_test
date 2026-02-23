7af489bfe239f038369f79bfdda4b639c9eee3c1
jackbongmin <rrrgk12@gmail.com>
2026-02-23 16:41:34 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#define fastio() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <cmath>
+
+using namespace std;
+
+// 15439
+
+int main() {
+    //fastio();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    
+
+    int N;
+
+    cin >> N;
+    cout << N * (N - 1);
+
+
+
+	return 0;
+}
