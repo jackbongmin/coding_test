49293091d6077d7e043c1bce15eab4fd1c009c49
jackbongmin <rrrgk12@gmail.com>
2026-03-11 22:09:48 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,34 @@
+
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <cctype>
+#include <unordered_map>
+
+
+using namespace std;
+
+
+int main() 
+{
+    //fastio();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << A*B;
+
+    return 0;
+    
+}
+
